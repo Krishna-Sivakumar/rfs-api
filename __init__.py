1dc127d9c7701bff1cd64d364c9f1b8e8572ab2c
Krishna <krishnasivaprogrammer@gmail.com>
2020-06-04 15:22:20 +0530

    changed database URI

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ import os, os.path
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///test.db"
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://hdqjgsykocnmbq:ba5abe0147435d8a43b8e1cc5e710d056ee422a2779231c5e70d61a24bc78284@ec2-54-75-229-28.eu-west-1.compute.amazonaws.com:5432/d1s0td7d585tps"
     db.init_app(app)
     return app
 
